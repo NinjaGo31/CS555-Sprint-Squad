234a37ace4a3205b8f9002a27eac440651778e00
ChaosIncarnate4 <69082212+ChaosIncarnate4@users.noreply.github.com>
2023-11-08 16:44:12 -0500

    Update Map.js

--- Additional Features/Map.js
+++ Additional Features/Map.js
@@ -1,5 +1,3 @@
 //window.open("https://www.creativeforce.com/wp-content/uploads/2015/12/hoboken-jersey-city-downtown-map1.jpg");
 //open a map of Hoboken (and Jersey City)
 //have overlaying icons over notable areas
-//upon clicking, pop up image and description of area
-//click 'x' button or outside of box to close it
