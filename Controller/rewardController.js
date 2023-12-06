eb0aa9ccfb2d94efb949cd49287ed20c050829dc
SurajSanjaySingh <singhsurajs1311@gmail.com>
2023-12-06 17:50:24 -0500

    reward

--- Controller/rewardController.js
+++ Controller/rewardController.js
@@ -1,61 +1,130 @@
+//this is the file related to reward system
+
+
+// Importing the reward model from rewardModel.js
+
 import { Reward as rewardModel } from "../Model/rewardModel.js";
+
+// Get All Reward Functions
+
 async function getAllRewards(req, res, next) {
+
   try {
+
     const rewards = await rewardModel.find();
+
     res.status(200).json({
+
       status: "success",
+
       results: rewards.length,
+
       data: { rewards },
+
     });
+
   } catch (error) {
+
     const err = error;
+
     err.statusCode = 404;
+
     next(err);
+
   }
 }
+
+// Get Reward for one Task
+
 async function getReward(req, res, next) {
+
   try {
+
     const rewards = await rewardModel.findById(req.params.id);
+
     res.status(200).json({
+
       status: "success",
+
       results: rewards.length,
+
       data: { rewards },
+
     });
+
   } catch (error) {
+
     const err = error;
+
     err.statusCode = 404;
+
     next(err);
+
   }
 }
+
+// Update The Reward Function 
+
 async function updateReward(req, res, next) {
+
   // to do
   try {
+
     const rewards = await rewardModel.findByIdAndUpdate(req.params.id, req.body, {
+
       new: true,
+
       runValidators: true,
+
     });
+
     res.status(200).json({
+
       status: "success",
+
       results: rewards.length,
+
       data: { rewards },
+
     });
+
   } catch (error) {
+
     const err = error;
+
     err.statusCode = 404;
+
     next(err);
+
   }
 }
+
+//Deleting the Reward Function 
+
 async function deleteReward(req, res, next) {
+
   try {
+
     await rewardModel.findByIdAndDelete(req.params.id);
+
     res.status(204).json({
+
       status: "success",
+
       data: null,
+
     });
+
   } catch (error) {
+
     const err = error;
+
     err.statusCode = 404;
+
     next(err);
+
   }
 }
+
+
 export { getAllrewards, getReward, updateReward, deleteReward };
--- Model/rewardModel.js
+++ Model/rewardModel.js
@@ -1,18 +1,42 @@
+//This is the reward Model file
+
+// Importing mongoose 
+
 import mongoose from "mongoose";
 
+
 const rewardSchema = new mongoose.Schema({
+
   name: {
+
     type: String,
+
     required: [true, "A reward must have a name"],
+
   },
+
+  // Name for the reward
+   
   points: {
+
     type: Number,
+
     required: [true, "A reward must have a point value"],
+
   },
+
+  //Point value of the reward 
+
   description: {
+
     type: String,
+
     required: [true, "A reward must have a description"],
+
   },
+
+  //description of the reward
+
 });
 
 const Reward = mongoose.model("Reward", rewardSchema);
