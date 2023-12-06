98dd69cedc51d77ab2721b868f45f2f856354f8b
ShanalDivyansh <73211821+ShanalDivyansh@users.noreply.github.com>
2023-12-06 17:45:10 -0500

    Update MapContainer.jsx

--- View/React/scavenger-hunt/src/components/MapContainer.jsx
+++ View/React/scavenger-hunt/src/components/MapContainer.jsx
@@ -1,14 +1,27 @@
-import HamburgerMenu from "./HamburgerMenu";
+import FilterHuntFom from "./FilterHuntFom";
+import { RandomAvatar } from "react-random-avatars";
 
 function MapContainer(props) {
   return (
     <div className="mapContainer">
       <nav>
-        <p>User name</p>
-        <p>Current scavenger hunt name</p>
-        <HamburgerMenu/>
+        <RandomAvatar name={props.charecterCreation.userName} size={25} />
+        <p>{props.charecterCreation.userName}</p>
+        <p
+          onClick={() => {
+            window.location.reload(false);
+          }}
+        >
+          logout
+        </p>
       </nav>
       {props.children}
+      {props.newUser && (
+        <FilterHuntFom
+          charecterCreation={props.charecterCreation}
+          dispatchFn={props.dispatchFn}
+        />
+      )}
     </div>
   );
 }
